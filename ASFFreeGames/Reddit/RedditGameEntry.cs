ec987c5d7ffee3882dde092514142bbf9e6cb623
maxisoft <maxisoft.eu@gmail.com>
2024-06-16 10:46:01 +0200

    Use OrderedDictionary instead of bloom filters in Reddit helper Minor changes here and there to disable explicit warning

--- ASFFreeGames/Reddit/RedditGameEntry.cs
+++ ASFFreeGames/Reddit/RedditGameEntry.cs
@@ -1,12 +1,12 @@
 ﻿namespace Maxisoft.ASF.Reddit;
 
 public readonly record struct RedditGameEntry(string Identifier, ERedditGameEntryKind Kind, long Date) {
-	public bool IsFreeToPlay => Kind.HasFlag(ERedditGameEntryKind.FreeToPlay);
-
 	/// <summary>
-	/// Indicates that the entry a DLC or a required game linked to a free DLC entry
+	///     Indicates that the entry a DLC or a required game linked to a free DLC entry
 	/// </summary>
 	public bool IsForDlc => Kind.HasFlag(ERedditGameEntryKind.Dlc);
+
+	public bool IsFreeToPlay => Kind.HasFlag(ERedditGameEntryKind.FreeToPlay);
 
 	public void Deconstruct(out string identifier, out long date, out bool freeToPlay, out bool dlc) {
 		identifier = Identifier;
